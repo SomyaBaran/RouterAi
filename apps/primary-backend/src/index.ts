d58887f574669b17f3cf0b36b0d571bcbbf87f0b
SomyaBaran <somyabaranwal6664@gmail.com>
2026-06-11 17:51:17 +0530

    dependencies and imports

--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
+import { app as authApp } from "./modules/auth"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").use(authApp).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
