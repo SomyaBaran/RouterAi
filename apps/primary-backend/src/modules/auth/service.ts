98e1c1518e9c22751d6d4e4ba9f78b338e5bb7de
SomyaBaran <somyabaranwal6664@gmail.com>
2026-06-12 19:27:32 +0530

    signin route

--- apps/primary-backend/src/modules/auth/service.ts
+++ apps/primary-backend/src/modules/auth/service.ts
@@ -3,12 +3,35 @@ import { prisma } from "db";
 export abstract class AuthService {
     static async signup(email: string, password: string): Promise<string> {
         const user = await prisma.user.create({
-            data: { email, password }
-        });
+            data: {
+                email,
+                password: await Bun.password.hash(password)
+            }
+        })
         return user.id.toString()
     }
-    static async signin(username: string, password: string): Promise<string> {
-        // const ok = await prisma.user.findFirst
-        return "123"
+    static async signin(email: string, password: string): Promise<{correctCredentials: boolean, userId?: string}> {
+        const user = await prisma.user.findFirst({
+            where: {
+                email
+            }
+        })
+        if (!user) {
+            return { correctCredentials: false };
+        }
+        if (!await Bun.password.verify(password, user.password)) {
+            return { correctCredentials: false };
+        }
+        return { correctCredentials: true, userId: user.id.toString() };
+    } 
+    static async getUserDetails(id: number) {
+        return prisma.user.findFirst({
+            where: {
+                id
+            },
+            select: {
+                credits: true
+            }
+        })
     }
 }
